257283e37178aebc0379c9ddd7cf521eb77c0119
shuvakarmakar <shuva15-7953@diu.edu.bd>
2023-11-06 11:23:54 +0600

    Image Import To the ImageGallery Component

--- src/Components/ImageGallery/ImageGallery.jsx
+++ src/Components/ImageGallery/ImageGallery.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 
+import './ImageGallery.css'
+
+import image1 from '../../assets/images/image-1.webp'
+import image2 from '../../assets/images/image-2.webp'
+import image3 from '../../assets/images/image-3.webp'
+import image4 from '../../assets/images/image-4.webp'
+import image5 from '../../assets/images/image-5.webp'
+import image6 from '../../assets/images/image-6.webp'
+import image7 from '../../assets/images/image-7.webp'
+import image8 from '../../assets/images/image-8.webp'
+import image9 from '../../assets/images/image-9.webp'
+import image10 from '../../assets/images/image-10.jpeg'
+import image11 from '../../assets/images/image-11.jpeg'
+import addImage from '../../assets/images/Add Image.png'
+
+
 const ImageGallery = () => {
     return (
         <div>
-            
+            <h1>This is Image Gallery Component</h1>
         </div>
     );
 };
